b79044a819af93408239ce54111adcfd2d45d47c
joako <joaquinbasas@yahoo.com>
2023-11-12 21:12:29 -0500

    avion en el geAliados

--- War-Tragedy/GeAliados.h
+++ War-Tragedy/GeAliados.h
@@ -3,11 +3,14 @@
 #include "Mono.h"
 #include "Bala.h"
 #include <vector>
+#include "AvionAliado.h"
 using namespace std;
 class GeAliado {
 private:
 	int contador;
 	vector<Mono*>listaMono;
+	AvionAliado* avion;
+
 public:
 	GeAliado(){
 		contador = 0;
@@ -22,6 +25,12 @@ public:
 		Mono* mono = new Mono(260, 210);
 		listaMono.push_back(mono);
 	}
+	/*
+	* void moverAliados(BufferedGraphics^ bg, Bitmap^ monoBM, Rectangle monoE, Bitmap^ bala, Rectangle balaE, int avX, int avY) {
+		moverMono(bg,monoBM,monoE,bala,balaE,avX,avY);
+		moverAvion();
+	}
+	*/
 
 	void moverMono(BufferedGraphics^ bg, Bitmap^ monoBM, Rectangle monoE, Bitmap^bala, Rectangle balaE, int avX, int avY) {
 		for (int i = 0; i < listaMono.size(); i++) {
@@ -30,6 +39,12 @@ public:
 				listaMono.at(i)->disparar(avX, avY);
 			}
 			listaMono.at(i)->moverB(bg, bala, balaE);
+		}
+	}
+	void moverAvion(BufferedGraphics^ bg, Bitmap^ aviBM, Rectangle aviE, Bitmap^ bala, Rectangle balaE) {
+		avion->mover(bg,aviBM,aviE);
+		if (contador % 8 == 0) {
+			avion->disparar();
 		}
 	}
 };
