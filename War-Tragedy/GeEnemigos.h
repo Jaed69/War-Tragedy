ae768005af60e2b828e93aa0e27edbe6e28ff650
joako <joaquinbasas@yahoo.com>
2023-11-18 23:08:50 -0500

    la serpiente no buk when vbalas.size()=0

--- War-Tragedy/GeEnemigos.h
+++ War-Tragedy/GeEnemigos.h
@@ -100,10 +100,12 @@ public:
 	}
 
 	void balasdejugadorcolisionanconserpiente(Jugador* ju) {
-		for (int i = 0; i < serpientes.size(); i++) {
-			if (ju->colBala(serpientes.at(i)->getFHB())) {//poner q pasa si serpiente colisiona con balas
-				serpientes.at(i)->setx(1250);
-				serpientes.at(i)->sety(500);
+		if (ju->getbulletsize() != 0) {
+			for (int i = 0; i < serpientes.size(); i++) {
+				if (ju->colBala(serpientes.at(i)->getFHB())) {//poner q pasa si serpiente colisiona con balas
+					//serpientes.at(i)->setx(1250); //cambiar esto jijijiji
+					//serpientes.at(i)->sety(500);
+				}
 			}
 		}
 	}
